39e6cacb6bef03ed04167105d68e844ad26eb1d9
michael-genus-pic <michael.genuspic@gmail.com>
2024-08-02 13:43:30 -0500

    adding a new network, local/docker

--- utils/config.py
+++ utils/config.py
@@ -1,13 +1,12 @@
-
+isInDocker = False
 hostName = "192.168.0.233"
 config = {
-    # "retryWait": [5, 10, 30, 60, 120, 300, 600, 1800],
-    "retryWait": [5, 5,5,5,5,5,5,5,5],
+    "retryWait": [5, 10, 30, 60, 120, 300, 600, 1800],
     "db": {"url": f"mongodb://{hostName}:27017/"},
     "apis":{
-        "animal": {"url":f"http://{hostName}/animal"},
-        "litter": {"url":f"http://{hostName}/litter"},
-        "notification": {"url":f"http://{hostName}/notification"},
-        "versioning": {"url":f"http://{hostName}/versioning"}
+        "animal": {"port": 3000},
+        "litter": {"port": 3001},
+        "notification": {"port": 3002},
+        "versioning": {"port": 3003}
     }
 }
